b519c1cf3f7c80316654d138feb51342e4c7074f
hellomaruf <marufahamed1110@gmail.com>
2025-01-03 23:03:51 +0600

    install shadcn and add dark mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import navlogo from '../assets/images/task.webp'
+import { ModeToggle } from './mode-toggle'
 
 
 function Navbar() {
@@ -6,6 +7,9 @@ function Navbar() {
     <div className="max-w-7xl mx-auto text-2xl my-3 flex items-center gap-1">
     <img className='w-[50px] h-[50px] object-cover' src={navlogo} alt=""  />
     <span className="font-bold ">Task</span>Master
+    <div className="ml-auto">
+    <ModeToggle/>
+    </div>
    </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,15 @@ import { Provider } from 'react-redux'
 import { store } from './assets/redux/store.ts'
 import { RouterProvider } from 'react-router-dom'
 import routes from './routers/route.tsx'
+import { ThemeProvider } from './providers/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Provider store={store}>
-    {/* <App /> */}
-    <RouterProvider router={routes}>
-
-    </RouterProvider>
-    </Provider>
-  </StrictMode>,
+    <ThemeProvider>
+      <StrictMode>
+        <Provider store={store}>
+        <RouterProvider router={routes}>
+        </RouterProvider>
+        </Provider>
+      </StrictMode>
+    </ThemeProvider>
 )
